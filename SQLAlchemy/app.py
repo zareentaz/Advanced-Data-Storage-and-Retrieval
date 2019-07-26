0d62ce825a858eb07154a55936278cb50b752982
zareen <zareentazshukur@gmail.com>
2019-07-26 18:49:16 -0500

    updating err bar

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -1,15 +1,50 @@
 # 1. import Flask
-from flask import Flask
+import numpy as np
 
-# 2. Create an app, being sure to pass __name__
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
+
+#################################################
+# Flask Routes
+#################################################
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
     return "Welcome to my 'Home' page!"
+
+@app.route("/api/v1.0/precipitation")
+def precipitaton():
+    print("")
+    return "Welcome"
 
 
 # 4. Define what to do when a user hits the /about route
