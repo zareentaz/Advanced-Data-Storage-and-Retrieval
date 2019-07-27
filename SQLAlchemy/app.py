15ae9e3097e36c96f100dcb332adf4ec77ff7878
zareen <zareentazshukur@gmail.com>
2019-07-27 00:20:43 -0500

    adding precipitaion route

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -1,26 +1,38 @@
 # 1. import Flask
+from flask import Flask, jsonify
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-Passenger = Base.classes.passenger
+keys = Base.classes.keys()
+
+global Measurement
+if "measurement" not in keys:
+    print("measurement does not exist in keys. Exiting the program")
+    exit(0)
+else:    
+    print("measurement exists in keys. Proceeding...")
+    # Save reference to the table
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -39,20 +51,52 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+    return (
+            f"Available Routes:<br/>"
+            f"/api/v1.0/precipitation<br/>"
+            f"/api/v1.0/stations<br/>"
+            f"/api/v1.0/tobs"       
+    ) 
 
 @app.route("/api/v1.0/precipitation")
-def precipitaton():
-    print("")
-    return "Welcome"
+def precipitation():
+    latest_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+    year_ago = dt.datetime.strptime(latest_date,'%Y-%m-%d') - dt.timedelta(days=365)
+    date_prcp_measure=session.query(Measurement.date,Measurement.prcp).\
+                       filter(Measurement.date >= year_ago).\
+                        order_by(Measurement.date).all()
+    
+   # Create a dictionary from the row data   
+    date_prcp = []
+    for date, prcp in date_prcp_measure:
+        Measurement_dict = {}
+        Measurement_dict["date"] = date
+        Measurement_dict["prcp"] = prcp
+        date_prcp.append(Measurement_dict)
+
+    return jsonify(date_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    station=session.query(Station.station).all()
+    
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(station))
+    
+    return jsonify(all_stations)
 
 
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+@app.route("/api/v1.0/tobs")
+def tobs():
+    latest_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+    year_ago = dt.datetime.strptime(latest_date,'%Y-%m-%d') - dt.timedelta(days=365)
+    date_tobs=session.query(Measurement.date,Measurement.tobs).\
+              filter(Measurement.date >= year_ago).\
+              order_by(Measurement.date).all()
+  
+    tobs=list(np.ravel(date_tobs))
+    return jsonify(tobs)
 
-
+           
 if __name__ == "__main__":
     app.run(debug=True)
